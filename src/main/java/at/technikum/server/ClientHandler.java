f90e88f5968ad8817ce5efe484f7b73a43bbeb99
Michael Strommer <69633126+mstrommer@users.noreply.github.com>
2024-09-26 12:40:54 +0200

    Reading from socket added.

--- src/main/java/at/technikum/server/ClientHandler.java
+++ src/main/java/at/technikum/server/ClientHandler.java
@@ -1,10 +1,14 @@
 package at.technikum.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
 
     private final Socket clientSocket;
+    private BufferedReader in;
 
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -13,5 +17,16 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         // Anfrage lesen, Request verarbeiten und Antwort zurücksenden
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null && !inputLine.isEmpty()){
+                builder.append(inputLine).append(System.lineSeparator());
+            }
+            // TODO process properly
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
